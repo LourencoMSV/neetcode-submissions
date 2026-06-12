55f731884c306f9d2aad8a39c035890bf88beef0
LourencoMSV <lourenco.mv@hotmail.com>
2026-06-12 07:00:36 +0200

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+
+        for s in strs:
+            encoded_string=encoded_string+"§§"+s
+        
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        split = s.split("§§")
+        return split[1:]
